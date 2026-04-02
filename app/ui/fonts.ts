7adf53c16c1fc64a800aeba68b5cfc45cceb62b4
JyckoS <jyckosianjaya@gmail.com>
2026-04-02 11:44:32 +0700

    feat: initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Fredoka } from 'next/font/google';
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const fredoka = Fredoka({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const inter = Inter({
+  subsets: ['latin'],
+});
